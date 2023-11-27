f5735657a41e27ba4da7cc22fb9c14e0e4da41b1
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-11-27 11:14:29 +0400

    fix: Task3, queens generator - full worked, use selection of random values :penguin:.

--- Homework_6/task3.py
+++ Homework_6/task3.py
@@ -1,40 +1,58 @@
 # Используйте генератор случайных чисел для случайной расстановки ферзей в
 # задаче выше. Проверяйте различный случайные варианты и выведите 4 успешных
 # расстановки.
-import random
-from itertools import combinations, combinations_with_replacement
-from random import randint, seed
+"""
+Eight queens problem.
+"""
+from itertools import combinations
+from random import randint
+from threading import Thread, current_thread
+
 from task2 import is_attacking
-from threading import Thread
-from pprint import pprint
+
+__QUEENS_COUNT = 8
+__BOARDS_COUNT = 4
 
 
-def __create_board(target: list, length: int, start: int) -> None:
+def __create_board(target: list, length: int) -> None:
+    """
+    The function writes 8 pairs of coordinates (range 1-8) to the target by
+    randomly selecting numbers. At the first intersection of the coordinates
+    specified in the 'is_attack' function, the search starts all over again.
+    :param target: list
+    :param length: count of coordinates pair
+    :return:
+    """
+    start = 1
     while len(target) < length:
-        target.append((randint(start, 8), randint(1, 8)))
+        target.append((start, randint(1, __QUEENS_COUNT)))
+        start += 1
         for q1, q2 in combinations(target, 2):
             if is_attacking(q1, q2):
-                try:
-                    target.remove(q2)
-                except ValueError:
-                    continue
-    print('Out')
+                target.clear()
+                start = 1
+    print(f'{current_thread().name}: \x1B[3mdone\x1B[0m', end='\r')
 
 
-def generate_boards():
+def generate_boards() -> list[list]:
+    """
+    Generates 4 sets of placements of 8 pieces on the board.
+    Each set runs in a separate thread.
+    :return: list of list
+    """
     board_list = []
     threads = {}
-    for i in (0, 2, 4, 6):
-        for j in range(i, i + 2):
-            board_list.append([])
-            threads[str(j)] = Thread(target=__create_board,
-                                     name=f'board_{j}',
-                                     args=(board_list[j], 8, j + 1))
-            threads[str(j)].start()
-        for thread in threads:
-            threads[thread].join()
+    for i in range(__BOARDS_COUNT):
+        board_list.append([])
+        threads[f'{i}'] = Thread(target=__create_board,
+                                name=f'board_{i}',
+                                args=(board_list[i], 8))
+        threads[f'{i}'].start()
+        for thread in threads.values():
+            thread.join()
     return board_list
 
 
 if __name__ == '__main__':
-    generate_boards()
+    for board in generate_boards():
+        print(board)
