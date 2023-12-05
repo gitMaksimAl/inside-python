b145679906ef9f57ed2744f616067ce86439e233
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-12-05 14:48:12 +0400

    ref: task 2 - done. All tests passed :heavy_check_mark:.

--- Homework_10/lottery_game.py
+++ Homework_10/lottery_game.py
@@ -5,20 +5,18 @@ class LotteryGame:
         self.number_set2 = ls2
 
     def compare_lists(self):
-        bingo = set()
+        bingo = []
         if self.number_set1 == self.number_set2:
-            print(f"Совпадающие числа: {self.number_set2}\n"
-                  f"Количество совпадающих чисел: {len(self.number_set2)}")
+            print(f"Совпадающие числа: {self.number_set1}\n"
+                  f"Количество совпадающих чисел: {len(self.number_set1)}")
             return self.number_set2
-        for i in range(len(self.number_set2)):
-            if self.number_set2[i] in self.number_set1:
-                bingo.add(self.number_set2[i])
-            if self.number_set1[i] in self.number_set2:
-                bingo.add(self.number_set1[i])
+        for item in self.number_set1:
+            if item in self.number_set2:
+                bingo.append(item)
         if bingo:
-            print(f"Совпадающие числа: {list(bingo)}\n"
+            print(f"Совпадающие числа: {bingo}\n"
                   f"Количество совпадающих чисел: {len(bingo)}")
-            return list(bingo)
+            return bingo
         else:
             print("Совпадающих чисел нет.")
             return None
